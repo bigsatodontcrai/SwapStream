bb7360c1d6b441dd9f89160c05c2aea9115269d2
Joshua-BerkleyKU <j708b537@ku.edu>
2022-04-16 21:29:09 -0500

    added the musickit stuff

--- src/app/apple-music-kit/apple-music-kit.component.ts
+++ src/app/apple-music-kit/apple-music-kit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as jose from 'jose'
 declare var MusicKit: any;
 
 @Component({
@@ -36,40 +37,37 @@ export class AppleMusicKitComponent implements OnInit {
   }
 
   setupapple() {
-    document.addEventListener('musickitloaded', function () {
-        // MusicKit global is now defined.
-
-        // Call configure() to configure an instance of MusicKit JS.
-        music = MusicKit.configure({
-        developerToken: '',
-        app: {
-            name: 'My Cool Web App',
-            build: '2022.4.11',
-        },
-        storefrontId: 'us'
-        }).then((result: any) => {
-          music = MusicKit.getInstance();
-          music.authorize()
-          .then(() => {
-            music.api.music('/v1/catalog/{{storefrontId}}/stations', {
-              'filter[identity]': 'personal',
-            }).then((output: any) => {
-              let stationID = output['data']['data'][0]['id'];
-              if (stationID) {
-                music.setQueue({ station: stationID }).then((queue: any) => {
-                  music.playNext({song: queue['_itemIDs'][0]})
-                  .then(() => music.play())
-                  .catch((error : any) => console.log(error));
-                });
-
-              } else {
-                console.error("Unable to retrieve station data.");
-              }
+    let music: any;
+    MusicKit.configure({
+    developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IlczU1pQRDMyUUMifQ.eyJpc3MiOiJRVE0zOExKUTNQIiwiaWF0IjoxNjQ5NzI2OTgzLCJleHAiOjE2NTE0NTQ5ODN9.5NYNeKqUBJCRLKhRdqhD3lFdIH02tnbk8RrW6LpinjH-EpVDF3lRfBwjjsrleXjK2l0QRKtmLGwBigWuc5bTaA',
+    app: {
+        name: 'My Cool Web App',
+        build: '2022.4.11',
+    },
+    storefrontId: 'us'
+    }).then((result: any) => {
+      music = MusicKit.getInstance();
+      music.authorize()
+      .then(() => {
+        music.api.music('/v1/catalog/{{storefrontId}}/stations', {
+          'filter[identity]': 'personal',
+        }).then((output: any) => {
+          let stationID = output['data']['data'][0]['id'];
+          if (stationID) {
+            music.setQueue({ station: stationID }).then((queue: any) => {
+              music.playNext({song: queue['_itemIDs'][0]})
+              .then(() => music.play())
+              .catch((error : any) => console.log(error));
             });
-          })
-          .catch(function(error: any) {
-            console.log(error);
-          });
+            
+          } else {
+            console.error("Unable to retrieve station data.");
+          }
         });
-  }
+      })
+      .catch(function(error: any) {
+        console.log(error);
+      });
+    });
+}
 }
