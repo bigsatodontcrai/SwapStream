01474a0517eadaecec75f5916420960847ae9b6e
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2022-04-16 21:12:57 -0500

    Music plays now :)

--- src/app/apple-music-kit/apple-music-kit.component.ts
+++ src/app/apple-music-kit/apple-music-kit.component.ts
@@ -15,10 +15,6 @@ export class AppleMusicKitComponent implements OnInit {
   }
   
   setupapple() {
-    // document.addEventListener('musickitloaded', function () {
-        // MusicKit global is now defined.
-  
-        // Call configure() to configure an instance of MusicKit JS.
         let music: any;
         MusicKit.configure({
         developerToken: 'eyJhbGciOiJFUzI1NiIsImtpZCI6IlczU1pQRDMyUUMifQ.eyJpc3MiOiJRVE0zOExKUTNQIiwiaWF0IjoxNjQ5NzI2OTgzLCJleHAiOjE2NTE0NTQ5ODN9.5NYNeKqUBJCRLKhRdqhD3lFdIH02tnbk8RrW6LpinjH-EpVDF3lRfBwjjsrleXjK2l0QRKtmLGwBigWuc5bTaA',
@@ -26,17 +22,30 @@ export class AppleMusicKitComponent implements OnInit {
             name: 'My Cool Web App',
             build: '2022.4.11',
         },
-        storefrontId: '1'
+        storefrontId: 'us'
         }).then((result: any) => {
           music = MusicKit.getInstance();
           music.authorize()
           .then(() => {
-            music.player.play();
+            music.api.music('/v1/catalog/{{storefrontId}}/stations', {
+              'filter[identity]': 'personal',
+            }).then((output: any) => {
+              let stationID = output['data']['data'][0]['id'];
+              if (stationID) {
+                music.setQueue({ station: stationID }).then((queue: any) => {
+                  music.playNext({song: queue['_itemIDs'][0]})
+                  .then(() => music.play())
+                  .catch((error : any) => console.log(error));
+                });
+                
+              } else {
+                console.error("Unable to retrieve station data.");
+              }
+            });
           })
           .catch(function(error: any) {
             console.log(error);
           });
         });
-    // });
   }
 }
