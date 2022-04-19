d22855ac6d4af6b494295855b3f5d3368fea48b2
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2022-04-19 15:59:50 -0500

    MusicKit can now be used

--- src/app/apple-music-kit/apple-music-kit.component.ts
+++ src/app/apple-music-kit/apple-music-kit.component.ts
@@ -9,18 +9,59 @@ declare var MusicKit: any;
 })
 export class AppleMusicKitComponent implements OnInit {
 
-
-
   Devtoken = '';
   privateKeystring = '-----BEGIN PRIVATE KEY----- MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQg8OljcWCOgxqqeqfDzxLQhGi5ibIscIGvyBYMD76VuNCgCgYIKoZIzj0DAQehRANCAATcbMVuB26hZ81i8E0KuzMD3HmXgXSIXV2NXDaqeuQgRapIRwHTOAVkI5nERowNgODqDL1DXRmyOpUNgjXEsbWs -----END PRIVATE KEY-----';
-
-  constructor() { }
-
-  ngOnInit(): void {
-    this.creatdevtoken()
+  appleMusicAPI : any;
+  musicPlaying = false;
+  constructor() { 
   }
 
-  async creatdevtoken()
+  ngOnInit(): void {
+    this.appleMusicAPI = {};
+    this.createdevtoken();
+  }
+
+  setMusicKitInstance(kit: any): any {
+    this.getMusicKitInstance = () => { return kit };
+  }
+
+  // This function is defined within setupapple
+  getMusicKitInstance = () : any => {};
+
+  handlePlayButtonClicked() {
+    this.queueSongsFromUserStation(); // THIS IS ONLY HERE FOR NOW UNTIL WE FIGURE OUT HOW TO PICK SPECIFIC SONGS
+    let music = this.getMusicKitInstance();
+    if (this.musicPlaying === true) {
+      music.pause();
+    } else {
+      music.play();
+    }
+  }
+
+  queueSongsFromUserStation() {
+    let music = this.getMusicKitInstance();
+    music.api.music('/v1/catalog/{{storefrontId}}/stations', {
+      'filter[identity]': 'personal',
+    }).then((output: any) => {
+      let stationID = output['data']['data'][0]['id'];
+      if (stationID) {
+        music.setQueue({ station: stationID }).then((queue: any) => {
+          music.playNext({song: queue['_itemIDs'][0]})
+          .then(() => {
+            //music.play();
+          })
+          .catch((error : any) => console.error(error));
+        });
+        
+      } else {
+        console.error("Unable to retrieve station data.");
+      }
+    });
+  }
+
+
+
+  async createdevtoken()
   {
     let datetime = Date.parse(Date())/1000;
     const ecPrivateKey = await jose.importPKCS8(this.privateKeystring, 'ES256')
@@ -32,7 +73,6 @@ export class AppleMusicKitComponent implements OnInit {
     .setExpirationTime('1d')
     .sign(ecPrivateKey)
     .then((jwt : string) => {
-      console.log(jwt);
       this.setupapple(jwt);
     });
   }
@@ -46,28 +86,12 @@ export class AppleMusicKitComponent implements OnInit {
         build: '2022.4.11',
     },
     storefrontId: 'us'
-    }).then((result: any) => {
-      music = MusicKit.getInstance();
+    }).then((instance: any) => {
+      music = instance;
       music.authorize()
-      .then(() => {
-        music.api.music('/v1/catalog/{{storefrontId}}/stations', {
-          'filter[identity]': 'personal',
-        }).then((output: any) => {
-          let stationID = output['data']['data'][0]['id'];
-          if (stationID) {
-            music.setQueue({ station: stationID }).then((queue: any) => {
-              music.playNext({song: queue['_itemIDs'][0]})
-              .then(() => music.play())
-              .catch((error : any) => console.log(error));
-            });
-            
-          } else {
-            console.error("Unable to retrieve station data.");
-          }
-        });
-      })
+      .then(() => this.setMusicKitInstance(music))
       .catch(function(error: any) {
-        console.log(error);
+        console.error(error);
       });
     });
 }
